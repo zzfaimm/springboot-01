458c7012e56995f3f2f1e94b83be8b7ac6c95173
aima <481086571@qq.com>
2022-03-14 13:25:11 +0800

    first commit

---
+++ src/main/java/com/study/controller/BookController.java
@@ -0,0 +1,16 @@
+package com.study.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/books")
+public class BookController {
+
+    @GetMapping
+    public String getById(){
+        System.out.println("springboot in running....");
+        return "springboot in running....";
+    }
+}
